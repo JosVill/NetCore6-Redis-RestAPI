4d810bb5d6ddd9fe16df2cd969450b18a851a3b1
JosVill <josvill004@gmail.com>
2022-04-25 15:57:39 -0500

    Rest API with chache service Final code

--- Data/RedisPlatformRepository.cs
+++ Data/RedisPlatformRepository.cs
@@ -20,19 +20,34 @@ namespace RedisAPI.Data
 
             var db = _redis.GetDatabase();
             var serialPlat = JsonSerializer.Serialize(plat);
-            db.StringSet(plat.Id, serialPlat);
+            
+            // db.StringSet(plat.Id, serialPlat);
+            // db.SetAdd("PatformSet", serialPlat);
+
+            db.HashSet($"hashplatform", new HashEntry[]{new HashEntry(plat.Id, serialPlat)});
         }
 
         public IEnumerable<Platform?>? GetAllPlatforms()
         {
             var db = _redis.GetDatabase();
-            var completeSet = db.GetAll();
+            // var completeSet = db.SetMembers("PlatformSet");
+          
+            var completeHash = db.HashGetAll("hashplatform");
+
+            if(completeHash.Length > 0)
+            {
+                var obj = Array.ConvertAll(completeHash, val => JsonSerializer.Deserialize<Platform>(val.Value)).ToList();
+                return obj;
+            }
+
+            return null;
         }
 
         public Platform? GetPlatformById(string id)
         {
             var db = _redis.GetDatabase();
-            var plat = db.StringGet(id);
+            // var plat = db.StringGet(id);
+            var plat = db.HashGet("hashplatform", id);
 
             if(!string.IsNullOrEmpty(plat))
             {
